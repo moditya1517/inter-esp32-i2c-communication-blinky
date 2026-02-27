ea78580abb0a76edbfa1cb7104f4a144b902a97f
modityaGupta <modityarocks@gmail.com>
2026-02-27 16:54:40 +0100

    ESP32-c6 code slave

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+// Code for the ESP32-WROOM-32 - SLAVE DEVICE
 #include <Arduino.h>
 #include "Wire.h"
 
@@ -53,3 +54,72 @@ void loop()
   }
   delay(500);
 }
+
+// Code for the ESP32-c6 - MASTER DEVICE
+// #include <Arduino.h>
+// #include "Wire.h"
+// #include <Adafruit_NeoPixel.h>
+
+// #define NUMPIXELS 1 // Number of LEDs
+// #define PIN 8
+// #define BUTTON_PIN 9
+
+// Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+// #define I2C_DEV_ADDR 0x55
+
+// uint32_t i = 0, r = 50, g = 0, b = 0;
+// int buttonState = 0;
+
+// void setup()
+// {
+//   pixels.begin();
+//   pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+//   Serial.begin(9600);
+//   Wire.begin(21, 22, 0UL);
+// }
+
+// void loop()
+// {
+//   buttonState = digitalRead(BUTTON_PIN);
+//   if (buttonState == LOW)
+//   {
+//     Wire.beginTransmission(0x55);
+//     Wire.print(1);
+//     Wire.endTransmission(true);
+//   }
+//   else
+//   {
+//     Wire.beginTransmission(0x55);
+//     Wire.print(0);
+//     Wire.endTransmission(true);
+//   }
+//   delay(500);
+
+//   uint8_t bytesReceived = Wire.requestFrom(I2C_DEV_ADDR, 1);
+
+//   if ((bool)bytesReceived)
+//   { // If received more than zero bytes
+//     uint8_t temp[bytesReceived];
+//     Wire.readBytes(temp, bytesReceived);
+//     char tt = (char)temp[0];
+//     if (tt == '1')
+//     {
+//       int temp = r;
+//       r = g;
+//       g = b;
+//       b = temp;
+
+//       pixels.setPixelColor(0, pixels.Color(r, g, b));
+//       pixels.show();
+//       delay(300);
+//     }
+//     else
+//     {
+//       pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+//       pixels.show();
+//       delay(300);
+//     }
+//   }
+// }
